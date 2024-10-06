72e5befc706cfe32f78b0050352fbdb898cd6468
Bruno Rodrigues Carloto <brunnocarlotosjob@gmail.com>
2024-10-06 01:39:05 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@ setup(
     name='regressao_linear_brasil',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'numpy',
-        # ... outras dependências
-    ]
     description='Uma classe para Regressão Linear em português, contendo as principais métricas da abordagem como métodos.',
     author='Bruno Rodrigues Carloto',
     author_email='brunnocarlotosjob@gmail.com',
