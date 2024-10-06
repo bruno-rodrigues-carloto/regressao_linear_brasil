bcf80a2f489599e47b6f259cea468de07df63c2e
Bruno Rodrigues Carloto <brunnocarlotosjob@gmail.com>
2024-10-06 01:04:30 -0300

    Add files via upload
    Setup do pacote modelo_linear_bruno_carloto.

---
+++ setup.py
@@ -0,0 +1,14 @@
+setup(
+    name='regressao_linear_brasil',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        # ... outras dependências
+    ]
+    description='Uma classe para Regressão Linear em português, contendo as principais métricas da abordagem como métodos.',
+    author='Bruno Rodrigues Carloto',
+    author_email='brunnocarlotosjob@gmail.com',
+    url='https://github.com/bruno-rodrigues-carloto/regressao-linear-brasil',
+    python_requires='>=3.6',
+)
